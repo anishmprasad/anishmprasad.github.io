77954b65658dd667d47c82fc191bac4f4da81c73
anish.m.prasad <anish.m.prasad@gmail.com>
2018-11-16 14:37:01 +0530

    test build

---
+++ precache-manifest.bd4d16b1cdbea0b0f5b8bd7d16b638ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "716256aedba89027832c",
+    "url": "/static/css/main.ee3a311c.chunk.css"
+  },
+  {
+    "revision": "716256aedba89027832c",
+    "url": "/static/js/main.716256ae.chunk.js"
+  },
+  {
+    "revision": "31b008c5b743ebe9ff6f",
+    "url": "/static/js/1.31b008c5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "707819c59533c1b819dd38446ae4f9c4",
+    "url": "/index.html"
+  }
+];
