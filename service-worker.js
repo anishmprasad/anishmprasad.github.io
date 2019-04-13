a32ea1cbb8deae883c96bdf6e04b8a375f6a9310
anish.m.prasad <anish.m.prasad@gmail.com>
2019-04-14 01:46:52 +0530

    build update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b0eb1e27b41e4c14eb93a16a97b01319.js"
+  "/precache-manifest.1da887005bf0724bf7146e99bc4c7f7b.js"
 );
 
 workbox.clientsClaim();
